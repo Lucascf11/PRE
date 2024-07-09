9ad9d5f850c29442186d19a4857066f189f1ff42
Lucas Costa Fontes <lucas.cf11@aluno.ifsc.edu.br>
2024-07-08 21:05:56 -0300

    Finalizada avaliação 4 de PRE

--- Avaliacoes/AV4/Avaliacao_4_Lucas.m
+++ Avaliacoes/AV4/Avaliacao_4_Lucas.m
@@ -6,6 +6,10 @@ N = 10000000; % Número de tentativas
 X1 = 3 * rand(1,N) - 2; % Gerando intervalo contínuo entre -2 e 1
 X2 = 3 * rand(1,N) - 2; % Gerando intervalo contínuo igual a X1
 
+%-----------------------------------------------------------------------------------
+% Variáveis aleatórias do enunciado
+%-----------------------------------------------------------------------------------
+
 Y1 = X1.^2;
 Y2 = X2.^2;
 Y3 = X1.*X2;
@@ -17,16 +21,19 @@ Z3 = Y1 + Y2 + Y3;
 %------------------------------------------------------------------------------------
 % Comando A
 %------------------------------------------------------------------------------------
-VetmediaY_sim = mean([Y1' Y2' Y3'])'
-VetmediaY_teo = [1;1;1/4]
 
-MatrizCovY_sim = cov([Y1' Y2' Y3'])'
-MatrizCovY_teo = [6/5,0,3/8;0,6/5,3/8;3/8,3/8,15/16]
+VetmediaY_sim = mean([Y1' Y2' Y3'])' % Vetor média de Y simulado
+VetmediaY_teo = [1;1;1/4] % Vetor média de Y calculado
+
+MatrizCovY_sim = cov([Y1' Y2' Y3'])' % Matriz covariância de Y simulada
+MatrizCovY_teo = [6/5,0,3/8;0,6/5,3/8;3/8,3/8,15/16] % Matriz covariância de Y calculada
+
 %------------------------------------------------------------------------------------
 % Comando B
 %------------------------------------------------------------------------------------
-VetmediaZ_sim = mean([Z1' Z2' Z3'])'
-VetmediaZ_teo = [1,2,9/4]'
 
-MatrizCovZ_sim = cov([Z1' Z2' Z3'])'
-MatrizCovZ_teo = [6/5,6/5,63/40;6/5,12/5,63/20;63/40,63/20,387/80]
+VetmediaZ_sim = mean([Z1' Z2' Z3'])' % Vetor média de Z simulado 
+VetmediaZ_teo = [1,2,9/4]' % Vetor média de Z calculado
+
+MatrizCovZ_sim = cov([Z1' Z2' Z3'])' % Matriz covariância de Z simulada
+MatrizCovZ_teo = [6/5,6/5,63/40;6/5,12/5,63/20;63/40,63/20,387/80] % Matriz covariância de Z calculada
